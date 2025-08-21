05e80007149716606754ea435dce4976fd02b2d5
kyuhwan <kyuhwann@gmail.com>
2025-08-22 01:13:53 +0900

    subscene load

---
+++ Client/Assets/Scripts/ECS/SubSceneLoadAuthoring.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class SubSceneLoadAuthoring : MonoBehaviour
+{
+    public bool AutoLoad = true;
+    public string SceneGuidString;
+}
+
+public struct SubSceneLoadConfig : IComponentData
+{
+    public Unity.Entities.Hash128 SceneGUID;
+    public bool AutoLoad;
+}
+
+public class SubSceneLoadBaker : Baker<SubSceneLoadAuthoring>
+{
+    public override void Bake(SubSceneLoadAuthoring authoring)
+    {
+        var entity = GetEntity(TransformUsageFlags.None);
+
+        Unity.Entities.Hash128 guid = default;
+
+        if (!guid.IsValid && !string.IsNullOrEmpty(authoring.SceneGuidString))
+        {
+            guid = new Unity.Entities.Hash128(authoring.SceneGuidString);
+        }
+
+        if (!guid.IsValid)
+            return;
+
+        AddComponent(entity, new SubSceneLoadConfig
+        {
+            SceneGUID = guid,
+            AutoLoad = authoring.AutoLoad
+        });
+    }
+}
---
+++ Client/Assets/Scripts/ECS/SubSceneLoaderSystem.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Scenes;
+
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+public partial struct SubSceneLoaderSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<SubSceneLoadConfig>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        if (!SystemAPI.TryGetSingletonEntity<SubSceneLoadConfig>(out var cfgEntity))
+            return;
+
+        var cfg = SystemAPI.GetComponent<SubSceneLoadConfig>(cfgEntity);
+        if (!cfg.AutoLoad || !cfg.SceneGUID.IsValid)
+            return;
+
+
+        var sceneEntity = SceneSystem.LoadSceneAsync(state.WorldUnmanaged, cfg.SceneGUID);
+
+        var tagEntity = state.EntityManager.CreateEntity();
+        
+        state.Enabled = false;
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+}
